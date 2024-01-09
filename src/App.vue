8080c13b1833e8c311d685e17fc72775ebac7912
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2024-01-09 12:06:31 -0300

    Iniciando Projeto
    Projeto do curso alura Desenvolvimento em Vue.

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Banner from "./components/Banner.vue";
+
+export default {
+  components: { Banner }
+}
+</script>
+
+<template>
+  <Banner />
+
+  <h1 class="titulo">
+    Texto do APP.vue
+  </h1>
+</template>
+
+<style scoped>
+.titulo {
+  color: yellow
+}
+</style>
