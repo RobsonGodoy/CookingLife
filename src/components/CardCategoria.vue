387522e4f75c2b409ddfa068c20fa91cf95038ac
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2024-01-12 12:36:33 -0300

    Update;

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+    props: {
+        categoria: { type: Object, required: true }
+    }
+}
+</script>
+
+<template>
+    <article class="categoria">
+        <header class="categoria__cabecalho">
+            <img :src="`/imagens/icones/categorias_ingredientes/${categoria.imagem}`" alt="" class="categoria__imagem">
+        </header>
+    </article>
+</template>
