38aaee4c96df333b6130faba94eb073a62a62424
Robson Junior <109744930+RobsonGodoy@users.noreply.github.com>
2024-01-14 11:46:09 -0300

    Finalização

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -3,7 +3,9 @@ import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
 import CardCategoria from './CardCategoria.vue';
 import BotaoPrincipal from './BotaoPrincipal.vue';
+
 export default {
+  name: 'SelecionarIngredientes',
   data() {
     return {
       categorias: [] as ICategoria[]
@@ -15,28 +17,34 @@ export default {
   components: { CardCategoria, BotaoPrincipal },
   emits: ['adicionarIngrediente', 'removerIngrediente', 'buscarReceitas']
 }
-
 </script>
 
 <template>
   <section class="selecionar-ingredientes">
-    <h1 class="cabecalho titulo-ingredientes"> Ingredientes </h1>
+    <h1 class="cabecalho titulo-ingredientes">Ingredientes</h1>
+
     <p class="paragrafo-lg instrucoes">
       Selecione abaixo os ingredientes que você quer usar nesta receita:
     </p>
 
     <ul class="categorias">
       <li v-for="categoria in categorias" :key="categoria.nome">
-        <CardCategoria :categoria="categoria" @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
-          @remover-ingrediente="$emit('removerIngrediente', $event)" />
+        <CardCategoria
+          :categoria="categoria"
+          @adicionar-ingrediente="$emit('adicionarIngrediente', $event)"
+          @remover-ingrediente="$emit('removerIngrediente', $event)"
+        />
       </li>
     </ul>
+
     <p class="paragrafo dica">
       *Atenção: consideramos que você tem em casa sal, pimenta e água.
     </p>
-    <BotaoPrincipal texto="Buscar Receitas" @click="$emit('buscarReceitas')"/>
+
+    <BotaoPrincipal texto="Buscar receitas!" @click="$emit('buscarReceitas')" />
   </section>
 </template>
+
 <style scoped>
 .selecionar-ingredientes {
   display: flex;
